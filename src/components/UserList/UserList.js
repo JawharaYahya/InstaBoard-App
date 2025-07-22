4d602c7a96da10a183be03361617eff13cc2a596
JawharaYahya <jawharashhaltough199@gmail.com>
2025-07-22 22:45:10 +0300

    update css

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -6,29 +6,52 @@ import axios from "axios";
 
 export default function UserList(){
     const[users, setUsers]=useState([]);
+    const [search,setsearch]=useState("");
+    const [darkMode,setDarkMode]=useState(false)
     const fetchUsers =async ()=>{
 const res= await axios.get("https://randomuser.me/api/?results=12");
 setUsers((currentUsers) => [...currentUsers, ...res.data.results]);
     };
+const filterUsers= users.filter((user)=>`${user.name.first} ${user.name.last}`.toLocaleLowerCase().includes(search.toLocaleLowerCase()));
     useEffect(()=>{
 
        fetchUsers(); 
     },[]);
 
     return (
-        <>
+       
+      <div className={darkMode ? "app dark-mode" : "app"}>
         <h2>Welcome to InstaBoard App</h2>
-      <button onClick={fetchUsers} id='LoadMoreBtn'>Load More</button>
+      <div className='searchSection'>
+      <h2>Find Your InstaBoard Bestie </h2>
+     <input type='text'
+     placeholder='Search by Name'
+     value={search}
+     onChange={(e)=>setsearch(e.target.value)}
+     id='search'
+     />
+     <button onClick={()=>setDarkMode(!darkMode)}>
+       {darkMode ? "🌞 Light Mode" : "🌙 Dark Mode"}
+     </button>
+
+     </div>
+    
+    
      <div id="userList">
-         {users.map((user) => (
+         {filterUsers.map((user) => (
         <UserCard
+        
           name={`${user.name.first} ${user.name.last}`}
           email={user.email}
           picture={user.picture.large}
         />
       ))}
     </div>
-    
-    </>
+    <div className='seeMorebtn'>
+     <h2>See More Users </h2>
+      <button onClick={fetchUsers} id='LoadMoreBtn'>Load More</button>
+      </div>
+      </div>
+   
     );
 } 
