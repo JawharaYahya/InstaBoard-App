e22c20142468939f1123c0466f943c10032d545d
JawharaYahya <jawharashhaltough199@gmail.com>
2025-07-30 00:34:53 +0300

    update2

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -2,21 +2,21 @@ import { Link } from 'react-router-dom';
 import './UserCard.css';
 import { useState } from "react";
 
-export default function UserCard({name, email,picture,user}) {
+export default function UserCard({user}) {
     const [likes , setLikes]= useState(0);
     const [showEmail , setshowEmail]= useState(false);
-
+    const {name, email,picture}=user;
     return(
 
 <div id="userCard" > 
-<h3>{name}</h3>
+<h3>{name.first} {name.last}</h3>
 <button onClick={()=>{setshowEmail(!showEmail)}}>Show/Hide Email</button>
 {showEmail && <p>{email}</p>}
-<img src={picture} alt="pic" width={150}/>
+<img src={picture.large} alt="pic" width={150}/>
 <h3>Likes {likes}</h3>
 <button onClick={()=>{setLikes(likes+1)}}>Like</button>
 <button onClick={()=>{setLikes(likes-1)}}>Unlike</button>
-<Link to={`/team/${user.login.uuid}`}>View Profile</Link>
+<Link to={`/team/${user.login.uuid}`} state={{user}}>View Profile</Link>
 
 </div>
 
